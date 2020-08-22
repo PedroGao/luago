922b0c9ca419cb52822d204847f267a71b9bada2
gaopeidong <gaopeidong@shimo.im>
2020-08-22 18:50:03 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# go 实现 lua 虚拟机
+
+
+
+## OTHERS
+
+- SEE https://github.com/zxh0/luago-book
---
+++ lua/ch02/foo_bar.lua
@@ -0,0 +1,11 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: shimo
+-- Date: 2020/8/22
+-- Time: 下午3:52
+-- To change this template use File | Settings | File Templates.
+--
+
+function foo()
+    function bar() end
+end
